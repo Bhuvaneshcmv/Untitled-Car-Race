38de17f9a0f4e6ef49a4bdccc481c1aaf95c397c
Bhuvanesh <bhuvaneshcmv@gmail.com>
2021-12-18 00:36:13 +0530

    Base Car Movement

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,39 +1,67 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Car : MonoBehaviour
-{
-    public List<AxleInfo> axleInfos; // the information about each individual axle
-    public float maxMotorTorque; // maximum torque the motor can apply to wheel
-    public float maxSteeringAngle; // maximum steer angle the wheel can have
-
-    public void FixedUpdate()
-    {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-
-        foreach (AxleInfo axleInfo in axleInfos)
-        {
-            if (axleInfo.steering)
-            {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
-            }
-            if (axleInfo.motor)
-            {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
-            }
-        }
-    }
-}
 
 [System.Serializable]
 public class AxleInfo
 {
     public WheelCollider leftWheel;
     public WheelCollider rightWheel;
-    public bool motor; // is this wheel attached to motor?
-    public bool steering; // does this wheel apply steer angle?
+    public bool motor;
+    public bool steering;
+}
+
+[System.Serializable]
+public class VisualWheelsInfo
+{
+    public GameObject leftWheel;
+    public GameObject rightWheel;
+}
+public class Car : MonoBehaviour
+{
+    [Header("AxleInfos and Visual Wheels Info should be in respective order")]
+    public List<AxleInfo> axleInfos;
+    public List<VisualWheelsInfo> visualWheelsInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider, GameObject wheel)
+    {
+        Transform visualWheel = wheel.transform;
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        //visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        if (axleInfos.Count != visualWheelsInfos.Count)
+        {
+            Debug.LogError("Visual Wheels and wheel colliders are not equal");
+            return;
+        }
+        for (int i=0;i< axleInfos.Count;i++)
+        {
+            if (axleInfos[i].steering)
+            {
+                axleInfos[i].leftWheel.steerAngle = steering;
+                axleInfos[i].rightWheel.steerAngle = steering;
+            }
+            if (axleInfos[i].motor)
+            {
+                axleInfos[i].leftWheel.motorTorque = motor;
+                axleInfos[i].rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfos[i].leftWheel, visualWheelsInfos[i].leftWheel);
+            ApplyLocalPositionToVisuals(axleInfos[i].rightWheel, visualWheelsInfos[i].rightWheel);
+        }
+    }
 }
